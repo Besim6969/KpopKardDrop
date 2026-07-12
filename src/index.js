1ecb514af4253c37b605fbd0a22817cb5f246b59
Besim6969 <bajrami.besim@hotmail.com>
2026-07-12 20:52:16 +0200

    Fix express middleware order in index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,9 @@ const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: '*' } });
 
 app.use(cors());
-app.use(express.json());:app.use(express.static(__dirname + '/../'));
+app.use(express.json());
+app.use(express.static(__dirname + '/../'));
+
 
 
 // Routes
