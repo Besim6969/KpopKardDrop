dd95d75cde322f912cd61a0f080302ce6ee41123
Besim6969 <bajrami.besim@hotmail.com>
2026-07-12 20:45:12 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: '*' } });
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json());:app.use(express.static(__dirname + '/../'));
+
 
 // Routes
 app.use('/api/auth',        require('./routes/auth'));
